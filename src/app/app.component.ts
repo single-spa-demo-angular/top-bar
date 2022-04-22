0e950d491f0b4cdbff84f0064919da5343369a3a
or33 <oreeshorah@gmail.com>
2022-04-22 11:44:56 +0000

    Top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { assetUrl } from 'src/single-spa/asset-url';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'appl-ng';
+  
+  url = assetUrl("../assets/images/seasons-logos.jpeg");
+  blackLogo = assetUrl("../assets/images/seasons-logos_black.png")
+  baseUrl = assetUrl("../assets/images/")
+
+  ngOnInit(): void {
+  }
 }
